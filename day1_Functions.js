6649e0bf9d2945965632f89b86513d148a61292f
Utsav Bhardwaj <utsavtinny1999@gmail.com>
2020-06-06 12:06:52 +0530

    Add files via upload

---
+++ day1_Functions.js
@@ -0,0 +1,8 @@
+function factorial(n) {
+    var fact=1;
+    for (var i = n; i > 0; i--) {
+        fact *= i;
+    }
+    return fact;
+
+ }
---
+++ day1_LetandConst.js
@@ -0,0 +1,8 @@
+let r = parseFloat(readLine());
+    const PI = Math.PI;
+
+    // Print the area of the circle:
+    console.log(PI * r * r);
+
+    // Print the perimeter of the circle:
+    console.log(PI * 2 * r);
